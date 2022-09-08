84b1e92b9c5c9299dc75cb45c929e99b1db9cfbd
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-08 16:26:00 +0300

    Готовое 1 задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startChangeColorBtnEl = document.querySelector('button[data-start]');
+const stopChangeColorBtnEl = document.querySelector('button[data-stop]');
+const backgroundColorEl = document.querySelector('body');
+let timerId = null;
+stopChangeColorBtnEl.disabled = true;
+
+startChangeColorBtnEl.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        let randomColor = getRandomHexColor();
+        backgroundColorEl.style.backgroundColor = randomColor;
+    }, 1000);
+    startChangeColorBtnEl.disabled = true;
+    stopChangeColorBtnEl.disabled = false;
+});
+
+stopChangeColorBtnEl.addEventListener('click', () => {
+    clearInterval(timerId)    
+    startChangeColorBtnEl.disabled = false;
+    stopChangeColorBtnEl.disabled = true;
+})
 
