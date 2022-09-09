261ab333fca18cb4225092d72f3e9c1170da577c
NikitaHordiienko <nikita.gordienko34@gmail.com>
2022-09-09 16:07:54 +0300

    Готовое 3 задание

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const generatorFormEl = document.querySelector('.form');
+const inputDelayEl = document.querySelector('input[name="delay"]');
+const inputStepEl = document.querySelector('input[name="step"]');
+const inputAmountEl = document.querySelector('input[name="amount"]');
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay)
+  });
+  
+  return promise;
+};
+
+generatorFormEl.addEventListener('submit', event => {
+  event.preventDefault()
+
+  let amountOfDealys = Number(inputDelayEl.value);
+  let amountOfSteps = Number(inputStepEl.value);
+
+  for (let i = 0; i < inputAmountEl.value; i += 1) {
+    createPromise(i + 1, amountOfDealys + i * amountOfSteps)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  };
+});
